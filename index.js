784ffb90ed7ab11bfa9172162e4e7302e82b1aa0
Luca Thompson <luca+github@urbanforest.dev>
2023-02-08 00:14:30 -0600

    added Not Implemented and Ping commands

--- index.js
+++ index.js
@@ -11,5 +11,37 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const {commandName} = interaction;
+    let ni = "This command isn't implemented yet!"; //Not Implemented
+
+    if(commandName === 'ping') {
+        await interaction.reply(`Hey hey! Latency is ${Date.now() - interaction.createdTimestamp}ms, and the API Latency is ${Math.round(client.ws.ping)}ms.`)
+    }
+    if(commandName === "add"){
+        await interaction.reply(ni)
+    }
+    if (commandName === "del"){
+        await interaction.reply(ni)
+    }
+    if (commandName === "loop"){
+        await interaction.reply(ni)
+    }
+    if (commandName === "shuffle"){
+        await interaction.reply(ni)
+    }
+    if (commandName === "pause"){
+        await interaction.reply(ni)
+    }
+    if (commandName === "play"){
+        await interaction.reply(ni)
+    }
+    if (commandName === "queue"){
+        await interaction.reply(ni)
+    }
+});
+
 // Log in to Discord with your client's token
 client.login(token);
+
