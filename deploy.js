514312753c41f255e9fe8f8161b7c56e5ae2b9b2
Luca Thompson <luca+github@urbanforest.dev>
2023-02-08 14:35:25 -0600

    changed command deployment method

--- deploy.js
+++ deploy.js
@@ -1,18 +1,57 @@
 const { REST, Routes } = require('discord.js');
 const { token } = require('./config.json');
-const fs = require('node:fs');
 
-const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [
+    new SlashCommandBuilder().setName('exit')
+        .setDescription('Leave the current channel.'),
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
-}
+    new SlashCommandBuilder().setName('loop')
+        .setDescription('Change queue loop settings.')
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("queue")
+				.setDescription("loops the current queue")
+		)
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("song")
+				.setDescription("loops the current song")
+		)
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("remove")
+				.setDescription("removes the loop from the queue or song")
+		),
 
-console.log(commands)
+    new SlashCommandBuilder().setName('pause')
+        .setDescription('Pause current song'),
+
+    new SlashCommandBuilder().setName('ping')
+        .setDescription('Check bot latency and API latency.'),
+
+    new SlashCommandBuilder().setName('play')
+        .setDescription('Add a song to the queue.'),
+
+    new SlashCommandBuilder().setName('queue')
+        .setDescription('List the current queue.'),
+
+    new SlashCommandBuilder().setName('resume')
+        .setDescription('Resume the current song.'),
+
+    new SlashCommandBuilder().setName('shuffle')
+        .setDescription('Shuffle the current queue.'),
+
+    new SlashCommandBuilder().setName('skip')
+        .setDescription('Skips the current song.'),
+
+    new SlashCommandBuilder().setName('about')
+        .setDescription('About shroom tunes.'),
+
+    new SlashCommandBuilder().setName('Help')
+        .setDescription('How to use shroom tunes.'),
+
+]
+    .map(command => command.toJSON());
 
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: '9' }).setToken(token);
