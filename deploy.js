6d0c29f96867a98c7cccf1e8da6f7c9110bbb3df
Luca Thompson <luca+github@urbanforest.dev>
2023-02-08 20:32:41 -0600

    fixed deployment script

--- deploy.js
+++ deploy.js
@@ -1,11 +1,14 @@
 const { REST, Routes } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { token } = require('./config.json');
 
 const commands = [
-    new SlashCommandBuilder().setName('exit')
+    new SlashCommandBuilder()
+        .setName('exit')
         .setDescription('Leave the current channel.'),
 
-    new SlashCommandBuilder().setName('loop')
+    new SlashCommandBuilder()
+        .setName('loop')
         .setDescription('Change queue loop settings.')
         .addSubcommand(subcommand =>
 			subcommand
@@ -23,53 +26,77 @@ const commands = [
 				.setDescription("removes the loop from the queue or song")
 		),
 
-    new SlashCommandBuilder().setName('pause')
+    new SlashCommandBuilder()
+        .setName('pause')
         .setDescription('Pause current song'),
 
-    new SlashCommandBuilder().setName('ping')
+    new SlashCommandBuilder()
+        .setName('ping')
         .setDescription('Check bot latency and API latency.'),
 
-    new SlashCommandBuilder().setName('play')
-        .setDescription('Add a song to the queue.'),
+    new SlashCommandBuilder()
+        .setName('play')
+        .setDescription('Add a song to the queue.')
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("song")
+				.setDescription("loops the current queue")
+                .addStringOption(option => option.setName("url").setDescription("the song's url").setRequired(true))
+		)
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("playlist")
+				.setDescription("loops the current queue")
+                .addStringOption(option => option.setName("url").setDescription("the playlist's url").setRequired(true))
+		)
+        .addSubcommand(subcommand =>
+			subcommand
+				.setName("search")
+				.setDescription("loops the current queue")
+                .addStringOption(option =>
+					option.setName("searchterms").setDescription("search keywords").setRequired(true)
+				)
+		),
 
-    new SlashCommandBuilder().setName('queue')
+    new SlashCommandBuilder()
+        .setName('queue')
         .setDescription('List the current queue.'),
 
-    new SlashCommandBuilder().setName('resume')
+    new SlashCommandBuilder()
+        .setName('resume')
         .setDescription('Resume the current song.'),
 
-    new SlashCommandBuilder().setName('shuffle')
+    new SlashCommandBuilder()
+        .setName('shuffle')
         .setDescription('Shuffle the current queue.'),
 
-    new SlashCommandBuilder().setName('skip')
+    new SlashCommandBuilder()
+        .setName('skip')
         .setDescription('Skips the current song.'),
 
-    new SlashCommandBuilder().setName('about')
+    new SlashCommandBuilder()
+        .setName('about')
         .setDescription('About shroom tunes.'),
-
-    new SlashCommandBuilder().setName('Help')
-        .setDescription('How to use shroom tunes.'),
 
 ]
     .map(command => command.toJSON());
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);
 
-// and deploy your commands!
 (async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+    try {
+        console.log('Started refreshing application (/) commands.');
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationCommands("1072746891382628413"),//, "1072748068413386752"),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
-	}
+        await rest.put(
+            Routes.applicationCommands("1072746891382628413"),
+            { body: commands },
+        );
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
 })();
+
+
+//1072746891382628413
